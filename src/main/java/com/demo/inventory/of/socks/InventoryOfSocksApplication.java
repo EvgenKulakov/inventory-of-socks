049ea0a7ecb6f1f02eefcbfc1b8f0efd24fbfc5d
Кулаков Евгений Александрович <000evgeniykulakov@gmail.com>
2024-12-20 14:54:57 +0300

    init commit

---
+++ src/main/java/com/demo/inventory/of/socks/InventoryOfSocksApplication.java
@@ -0,0 +1,13 @@
+package com.demo.inventory.of.socks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryOfSocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryOfSocksApplication.class, args);
+	}
+
+}
